1bd0d9a9c09008414ca7987fae44aaaee6d68e52
yura.matora11@gmail.com <yurii matora>
2020-12-10 00:15:05 +0200

    [TASK-01] Human password generator
     - Extend generator utils

--- src/main/java/com/yurwar/utils/GeneratorUtils.java
+++ src/main/java/com/yurwar/utils/GeneratorUtils.java
@@ -4,15 +4,28 @@ import com.yurwar.files.AbstractFileReader;
 import com.yurwar.files.DefaultFileReader;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public final class GeneratorUtils {
     public static final String VOWELS = "aeiouy";
-    public static final String consonants = "bcdfghjklmnpqrstvwxz";
-    public static final String alphabet = "abcdefghijklmnopqrstuvwxyz";
-
+    public static final String CONSONANTS = "bcdfghjklmnpqrstvwxz";
+    public static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+    public static final String UPPERCASE_NUM_SYMBOLS = ")!@#$%^&*(";
+    public static final Map<String, String> NUM_TO_UPPERCASE_SYMBOL = createNumToUppercaseSymbol();
+    public static final String DELIMITERS = "-_.";
+    public static final String OTHER_SYMBOLS = ";:?*=,";
     private static final AbstractFileReader<String> defaultFileReader = new DefaultFileReader();
+    public static final List<String> TOP_25_PASSWORDS =
+            defaultFileReader.readPasswordsFromFile("./src/main/resources/top25_passwords.csv");
 
+    private static Map<String, String> createNumToUppercaseSymbol() {
+        return IntStream.range(0, 10)
+                .mapToObj(num -> Map.entry(String.valueOf(num), UPPERCASE_NUM_SYMBOLS.split("")[num]))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
 
-    public static final List<String> TOP_25_PASSWORDS = defaultFileReader.readPasswordsFromFile("./src/main/resources/top25_passwords.csv");
+    private GeneratorUtils() {}
 
 }
