2e215693ca89ac4f0808ee7714f1d60322be39d0
Yurii Matora <yurii_matora@epam.com>
2020-12-10 03:11:10 +0200

    [TASK-01] Human password generator
     - Add hashes for generated passwords
     - Execute hashing in parallel

--- src/main/java/com/yurwar/hash/AbstractHashingStrategy.java
+++ src/main/java/com/yurwar/hash/AbstractHashingStrategy.java
@@ -2,21 +2,12 @@ package com.yurwar.hash;
 
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public abstract class AbstractHashingStrategy {
 
     public List<String> hash(List<String> passwords) {
 
-        return IntStream.range(0, passwords.size())
-                .peek(index -> {
-                    if (index % 10000 == 0) {
-                        System.out.println("Index =" + index);
-                    }
-                })
-                .mapToObj(index -> hash(passwords.get(index)))
-                .collect(Collectors.toList());
-//        return passwords.stream().map(this::hash).collect(Collectors.toList());
+        return passwords.stream().parallel().map(this::hash).collect(Collectors.toList());
     }
 
     protected abstract String hash(String password);
